98edb06438d6e67f82ab2c67480838d8b09f627e
Chase <chasedemaster.tech@gmail.com>
2022-08-12 11:33:27 -0500

    commit tree 2ac338aa164ae81ff8d00d5d8946a0df3d0a1693

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,11 @@ class PostsController < ApplicationController
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
     post.update(post_params)
 
     render json: post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate fiction_non
+    validates :content, length: { minimum, 100}
+
+    def fiction_non
+        if :category.downcase === "fiction" | "non-fiction"
+    end
 end
